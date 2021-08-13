0580d3b8d895de21d2fc177bb050f71b816efff1
Adamor Henner <adacandido9@gmail.com>
2021-08-13 11:40:26 -0300

    estudar

---
+++ src/main/java/ConnectionJDBC.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionJDBC {
+    public static void main(String[] args) throws SQLException {
+        String urlConnection = "jdbc:postgresql://localhost/inovationone";
+
+//        Connection conn = null;
+//
+//        try{
+//            conn = DriverManager.getConnection(urlConnection, "postgres","123456");
+//            System.out.println("SUCESSO!");
+//        } catch (Exception e ){
+//            System.out.println("Falha!");
+//            e.printStackTrace();
+//        } finally {
+//            conn.close();
+//        }
+
+
+        try(Connection conn = DriverManager.getConnection(urlConnection, "postgres", "123456")){
+            System.out.println("SUCESSO!");
+        } catch (Exception e){
+            System.out.println("Falhou!");
+        }
+    }
+}
